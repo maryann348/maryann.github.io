95e27e9a237d215c699ffa0f28ef8d4dd30de9a1
camaymaryann123@gmail.com <camaymaryann@123@gmail.com>
2019-09-06 17:51:27 +0800

    Initial Commits

--- controller.js
+++ controller.js
@@ -105,3 +105,48 @@ $('#btn-connect').on('click', function () {
 
 
 
+// client.subscribe("mqtt/demo")
+
+// btnPublish.on('click', function (e) {
+//   e.preventDefault();
+//   console.log("publish button clicked..")
+//   client.publish("mqtt/demo", "hi I'm Jessa Mae good Morning")
+
+//   client.on("message", function (topic, payload) {
+//     console.log([topic, payload].join(": "));
+//     // client.end();
+
+
+
+//   })
+//   // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+
+// })
+
+
+// // advance functionalities
+// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+// client.subscribe("mqtt/demo", function (err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("subscribed")
+//   }
+// })
+
+// client.on("connect", function(){
+//     console.log("Successfully connected");
+// })
+
+// client.on("message", function (topic, payload) {
+//   console.log([topic, payload].join(": "));
+//   client.end();
+// })
+
+// client.publish("mqtt/demo", "hello world!", function(err){
+//   if (err){
+//     console.log(err)
+//   } else {
+//     console.log("published")
+//   }
+// })
